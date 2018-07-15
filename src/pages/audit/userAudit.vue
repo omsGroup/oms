23459b2e0d1704d398e86a9b1cd2145253dc8ba1
baowenwu <2586224896@qq.com>
2018-07-15 21:59:44 +0800

    Merge branch 'master' of github.com:omsGroup/oms
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/pages/audit/userAudit.vue
+++ src/pages/audit/userAudit.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="user-audit-pages">
         <tablelist ref="tableList"
+                   :page-title="'用户审核'"
+                   :search-title="'用户审核搜索条件'"
                    :form="form"
-                   :data="[{checkEl:'',sort:'2',id:'22',nickname:'张张',identify:'321563258963145',applyTime:'2018-07-15 19:21',headPic:'../assets/images/056f814a17e86151a306028bee30add4.png',operate:''},{checkEl:'',sort:'3',id:'26',nickname:'张张',identify:'321563258963145',applyTime:'2018-07-15 19:21',headPic:'../assets/images/056f814a17e86151a306028bee30add4.png',operate:''}]"
+                   :data="[{sort:'2',id:'22',nickname:'张张',identify:'321563258963145',applyTime:'2018-07-15 19:21',headPic:'../assets/images/056f814a17e86151a306028bee30add4.png'},{sort:'3',id:'26',nickname:'张张',identify:'321563258963145',applyTime:'2018-07-15 19:21',headPic:'../assets/images/056f814a17e86151a306028bee30add4.png'}]"
                    :search-param="[{label:'用户昵称',prop:'nickname'},
                                    {label:'用户ID',prop:'id'},
                                    {label:'身份证号',prop:'identify'},
@@ -11,14 +13,15 @@
                    :button-group="[{label:'查询',methods:'search'},
                                    {label:'批量通过',methods:'agree'},
                                    {label:'批量拒绝',methods:'refuse'}]"
-                   :table-type="[{label:'',prop:'checkEl',type:'checkbox'},
-                                 {label:'序号',prop:'sort'},
-                                 {label:'用户ID',prop:'id'},
-                                 {label:'昵称',prop:'nickname'},
-                                 {label:'身份证号码',prop:'identify'},
-                                 {label:'申请时间',prop:'applyTime'},
-                                 {label:'身份证头像',prop:'headPic',type:'img'},
-                                 {label:'操作',prop:'operate'}]"/>   
+                   :table-type="[{label:'序号',prop:'sort',minWidth:70},
+                                 {label:'用户ID',prop:'id',minWidth:70},
+                                 {label:'昵称',prop:'nickname',minWidth:70},
+                                 {label:'身份证号码',prop:'identify',minWidth:100},
+                                 {label:'申请时间',prop:'applyTime',minWidth:100},
+                                 {label:'身份证头像',prop:'headPic',type:'img',minWidth:70}]"
+                   :table-option="[{label:'通过',methods:'agree',type:'success',span:12},
+                                   {label:'拒绝',methods:'refuse',type:'danger',span:12}]"
+                   :table-option-width="300"/>   
     </div>
 </template>
 <script>
