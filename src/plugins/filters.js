9a1a30042b17f8ccf11dbc8fc4cf6dcee2756695
baowenwu <2586224896@qq.com>
2018-07-15 21:36:55 +0800

    时间戳格式化

--- src/plugins/filters.js
+++ src/plugins/filters.js
@@ -1,4 +1,6 @@
 export function sex(val){
-    console.log(val)
-    return val ===1?'男':'女'
+    return val === 1?'男':'女'
+}
+export function accountType(val){
+    return val === 0 ?'普通用户':val === 1?'管理员':'超级管理员'
 }
